e720e6b4c572d70eb3b3afa39ba9d56f247c48be
yzh02U <83672268+yzh02U@users.noreply.github.com>
2025-02-07 23:01:41 -0300

    Update Tuya.tsx

--- src/Components/Tuya.tsx
+++ src/Components/Tuya.tsx
@@ -12,17 +12,16 @@ interface TokenData {
   tid: string;
 }
 
-function getToken() {
-  const [level, setLevel] = useState<TokenData | null>(null);
-  const p = "";
+function getCredentialAccess() {
+  const [access, setAccess] = useState<TokenData | null>(null);
   const getToken = async () => {
     try {
       const response = await fetch("/tuya-api/v1.0/token?grant_type=1", {
         method: "GET",
         headers: {
           client_id: "4fgcma3wh97nra5phuds",
-          sign: "D399C902EFB07A644D7795EDE51B3121B698B90012FBBE725F0B6140C7DFC17A",
-          t: "1738977996030",
+          sign: "EF3BB4EBAAE3882E2F5A7DD46B559BF4151B1A7090E5F65598A115AECF0FCBBD",
+          t: "1738979880201",
           sign_method: "HMAC-SHA256",
           nonce: "",
           stringToSign: "",
@@ -37,7 +36,7 @@ function getToken() {
 
       // Verificar que 'result' existe antes de actualizar el estado
       if (data && data.result) {
-        setLevel(data);
+        setAccess(data);
       } else {
         console.error("Respuesta inesperada de la API:", data);
       }
@@ -50,7 +49,7 @@ function getToken() {
     getToken();
   }, []);
 
-  return <p>{level && level.result ? level.result.uid : "Cargando..."}</p>;
+  return <p>{access && access.result ? access.result.uid : "Cargando..."}</p>;
 }
 
-export default getToken;
+export default getCredentialAccess;
